b9a57ae158480f2474bdba908d6c0c3b09b66410
ifouzezi <ifoghaleuzezi@gmail.com>
2026-02-10 15:21:40 +0100

    tailiwind config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,20 @@
 module.exports = {
   content: ["./dist/**/*.html"],
-
   theme: {
     extend: {
       colors: {
-        slate: {
-          900: "#1F314F",
-          500: "#68778D",
-          300: "#D5E1EF"
-        },
+        "qr-dark": "#1F314F",    // was slate-900
+        "qr-gray": "#68778D",     // was slate-500
+        "qr-light": "#D5E1EF",    // was slate-300
         white: "#FFFFFF"
       },
-
       fontFamily: {
         sans: ["Outfit", "ui-sans-serif", "system-ui"]
       },
-
       fontSize: {
         preset1: ["22px", { lineHeight: "120%", letterSpacing: "0px", fontWeight: "700" }],
         preset2: ["15px", { lineHeight: "140%", letterSpacing: "0.2px", fontWeight: "400" }]
       },
-
       spacing: {
         200: "16px",
         300: "24px",
@@ -29,6 +23,5 @@ module.exports = {
       }
     }
   },
-
   plugins: []
 }
